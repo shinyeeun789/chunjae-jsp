316af8a192ea1aad86fcbfbe838c380bc3121d3f
shinyeeun789 <tlsdpdms789@naver.com>
2023-08-22 15:31:22 +0900

    상품 기능 구현 중

---
+++ pro02/src/edu/chunjae/dto/Receive.java
@@ -0,0 +1,63 @@
+package edu.chunjae.dto;
+
+// 입고
+public class Receive {
+
+    private int rno;                // 입고 번호
+    private int pno;                // 입고 제품번호
+    private int amount;             // 입고량
+    private int rprice;             // 입고 가격
+    private String resdate;         // 입고일
+
+    public int getRno() {
+        return rno;
+    }
+
+    public void setRno(int rno) {
+        this.rno = rno;
+    }
+
+    public int getPno() {
+        return pno;
+    }
+
+    public void setPno(int pno) {
+        this.pno = pno;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getRprice() {
+        return rprice;
+    }
+
+    public void setRprice(int rprice) {
+        this.rprice = rprice;
+    }
+
+    public String getResdate() {
+        return resdate;
+    }
+
+    public void setResdate(String resdate) {
+        this.resdate = resdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Receive{" +
+                "rno=" + rno +
+                ", pno=" + pno +
+                ", amount=" + amount +
+                ", rprice=" + rprice +
+                ", resdate='" + resdate + '\'' +
+                '}';
+    }
+
+}
